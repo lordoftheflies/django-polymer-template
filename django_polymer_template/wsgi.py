6d54d697b20102906f5e68baa2ddfade91ffb804
lordoftheflies <heglas11@gmail.com>
2017-05-25 16:05:29 +0200

    Entry points with configuration.

--- django_polymer_template/wsgi.py
+++ django_polymer_template/wsgi.py
@@ -1,16 +1,32 @@
 """
-WSGI config for django_polymer_template project.
-
+WSGI config for nomono project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
 import os
+import dotenv
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_polymer_template.settings")
+try:
+    dotenv.read_dotenv(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+except Exception as e:
+    print(e)
+
+
+ENVIRONMENT = os.getenv('ENVIRONMENT')
+
+if ENVIRONMENT == 'STAGING':
+    settings = 'staging'
+elif ENVIRONMENT == 'PRODUCTION':
+    settings = 'production'
+else:
+    settings = 'development'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nomono.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', settings.title())
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
